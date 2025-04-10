75c2795e5a0c53c13811f8c1bf63fab9d988f2c0
dylan <burgesd5@wwu.edu>
2025-04-10 13:17:51 -0700

    BuzzButton fully functional

---
+++ BuzzButton/BuzzButton.ino
@@ -0,0 +1,41 @@
+
+/* Pin Definitions */
+// The button is pin A4 and the vibe board is pin 3
+int buttonPin = 4;
+int vibePin = 3;
+
+void setup()
+{
+  
+  //Button 
+  pinMode(buttonPin, INPUT);  // set the pin as an input
+  digitalWrite(buttonPin, HIGH);  // enable the pull-up resistor
+
+  //VIBE
+  pinMode(vibePin, OUTPUT);  // set the pin as an output
+
+  
+}
+
+void loop()
+{
+  
+  //if the button is being pressed
+     if(digitalRead(buttonPin) == 0) {
+         
+          //turn on vibe
+          digitalWrite(vibePin, HIGH);
+          
+          //give it a moment to vibrate
+          delay(1000);
+          
+          //turn it back off again
+          digitalWrite(vibePin, LOW);   
+                   
+     }
+
+     delay(1000);
+
+}
+
+
